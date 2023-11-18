cfc26274e6b77ea6f91fcbd1c7034b24f9d3262c
Aung Zaw Phyo <aungzawphyo1102@gmail.com>
2023-11-18 22:02:25 +0630

    first commit

---
+++ 01_basic/script.js
@@ -0,0 +1,41 @@
+// Variables
+
+let name = 'aungzawphyo';
+name = 'suthet';
+const age = 19;
+
+
+/*
+    'let' can update
+    'const' cannot update
+*/
+
+
+// opertar 
+// boolean => true | false
+
+let su_age = 19;
+let aung_age = 19;
+
+const check = su_age <= aung_age; // less than or equal
+let num1 = '2';
+let num2 = 3;
+let num3 = +num1 + +num2
+
+// ---------------- Day 2 ------------------
+
+const mgmgMark = 80;
+const kokoMark = 60;
+
+const compareMark = !(mgmgMark < kokoMark);
+
+/*
+    ==      => isTrue   (eg => 44 == '44' => true)  | can ignore data type (check value, not data type)
+    ===     => isTrue   (eg => 55 === '55'  => true) | check both value and data type
+
+    !=      => isFalse  (eg => 44 != '44' => false)
+    !==     => isFalse  (eg => 44 !== '44' => true)
+
+    ||      => OR operator      (eg => 33 == 33 || 44 == 45)  | if all false , return false
+    &&      => AND operator     (eg => 33 == 33 && 44 == 45)  | if all true , return true
+*/
