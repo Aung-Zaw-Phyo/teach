caa3285d97b9ca5d9c6e27189973ce9d7fe155c2
Aung Zaw Phyo <aungzawphyo1102@gmail.com>
2023-12-10 10:08:08 +0630

    loop practise

--- 01_basic/basic_2.js
+++ 01_basic/basic_2.js
@@ -1,15 +1,36 @@
-const persons = [
-    'aungaung',
-    'mgmg',
-    'zawzaw',
-    'mgmg',
-    'susu',
-    'koko'
+const persons = [{
+        name: 'Aung Zaw Phyo',
+        age: 20,
+        email: 'aungzawphyo@gmail.com',
+        profession: 'Programmer'
+    },
+    {
+        name: 'Su Thet Naing',
+        age: 13,
+        email: 'suthetnaing@gmail.com',
+        profession: 'Student'
+    },
+    {
+        name: 'Phyo Ei Zin',
+        age: 25,
+        email: 'phyoeizin@gmail.com',
+        profession: 'Designer'
+    },
+    {
+        name: 'Aung Phyo',
+        age: 28,
+        email: 'aungphyo@gmail.com',
+        profession: 'Engineer'
+    },
 ];
 
 // length = 4 , 0 1 2 3
 // i = 0 , 1 , 2 , 3
 
 for (let i = 0; i < persons.length; i++) {
-    console.log("'i' value is ", i, persons[i])
+    console.log('Name: ', persons[i].name)
+    console.log('Age: ', persons[i].age)
+    console.log('Email: ', persons[i].email)
+    console.log('Profession: ', persons[i].profession)
+    console.log('')
 }
